c64e2226cd9a437826cd8a7e942abff0bbad9fd0
Yannick PLASSIARD <plassiardyannick@gmail.com>
2017-12-29 18:06:31 +0100

    Little fix to avoid tips during a sayAll

--- addon/globalPlugins/tips/__init__.py
+++ addon/globalPlugins/tips/__init__.py
@@ -1,4 +1,3 @@
-# *-* coding: utf8 *-*
 ##
 ## Tips
 ## Show tips to help the user use NVDa more efficiently.
@@ -12,7 +11,7 @@ from NVDAObjects import NVDAObject
 import api
 import controlTypes
 import speech
-import globalPluginHandler, addonHandler
+import globalPluginHandler, addonHandler, sayAllHandler
 addonHandler.initTranslation()
 import wx
 import logHandler
@@ -20,6 +19,7 @@ import logHandler
 
 
 ignoredRoles = (controlTypes.ROLE_PANE, controlTypes.ROLE_WINDOW, controlTypes.ROLE_CLOCK)
+
 
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
     scriptCategory = _("TIPS")
@@ -47,13 +47,15 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 
     def sayTip(self):
         global ignoredRoles
+        if sayAllHandler.isRunning():
+            return
 
-        msg = None
         if self.obj is None:
             return
         if self.obj.role in ignoredRoles:
             return
-        speech.speakMessage(_("You are on a %s: %s") %(controlTypes.roleLabels[self.obj.role], self.explain()))
+        msg = self.explain()
+        speech.speakMessage(_("You are on a {elementType}: {elementMessage}").format(elementType=controlTypes.roleLabels[self.obj.role], elementMessage=msg))
 
     def explain(self):
         msg = ""
@@ -83,7 +85,7 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
                 msg = _("Press space to check.")
         elif self.obj.role is controlTypes.ROLE_RADIOBUTTON:
             if controlTypes.STATE_CHECKED not in self.obj.states:
-                msg = _("Press space to se;ect this option, or use the arrow keys to select another one in this group.")
+                msg = _("Press space to select this option, or use the arrow keys to select another one in this group.")
             else:
                 msg = _("Option selected: use the arrow keys to see and select other options.")
         elif self.obj.role is controlTypes.ROLE_SLIDER:
@@ -97,7 +99,7 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
         msg = u""
         if obj.role is controlTypes.ROLE_BUTTON:
             if obj.parent and obj.parent.__class__.__name__ is 'NotificationArea':
-                msg = _("You are in the notification area. This area contains icons of running programs and services allowing you to control their execution and settings. Use the left and right arrow keys to move beiween them and enter to activate the desired icon. You may also use the Application key (or context menu key) to open a pop-up menu containg more options regarding the currently focused icon. To quickly go to the notification area from anywhere, use the Windows+B shortcut.")
+                msg = _("You are in the notification area. This area contains icons of running programs and services allowing you to control their execution and settings. Use the left and right arrow keys to move bitween them and enter to activate the desired icon. You may also use the Application key (or context menu key) to open a pop-up menu containg more options regarding the currently focused icon. To quickly go to the notification area from anywhere, use the Windows+B shortcut.")
         if obj.role is controlTypes.ROLE_LISTITEM:
             if obj and obj.parent is not None and obj.parent.parent is not None and obj.parent.parent.__class__.__name__ == u'WindowRoot':
                 msg = _("You are on the Desktop, containing several icons to start programs or open documents. Arranged as a grid, you can move using arrow keys bitween them, and press enter to activate the desired one. You may also use the Application (or context menu) key to open a pop-up menu containing other options related to an icon. To quickly go to the Desktop from anywhere, simply use the Windows+D shortcut.")
